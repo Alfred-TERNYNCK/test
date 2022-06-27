a7f42de9d5c0f0c510db56f32cebfac179d000c4
Alfred-TERNYNCK <alfred.ternynck@epitech.eu>
2022-06-27 10:28:27 +0200

    []

---
+++ Calendar/requirement.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2022
+** requirement.c
+** File description:
+** requirement
+*/
+
+#ifndef REQUIREMENT_H_
+    #define REQUIREMENT_H_
+
+typedef struct list
+{
+void *data ;
+struct list *next ;
+} list_t;
+
+#endif
+
+int my_revlist_synthesis(list_t **begin)
+{
+    return 0;
+}
+
+list_t *my_findnodeeqinlist_synthesis(list_t *begin ,void *data_ref ,int (*cmp) (void *,void *))
+{
+    return;
+}
